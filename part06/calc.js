d756111512f16a8641be1c0111d8d6f836dcae0d
Nehasree08 <nehasrinivas08@gmail.com>
2024-05-11 19:19:25 +0530

    added more tags

---
+++ part06/calc.js
@@ -0,0 +1,10 @@
+ function add(x, y){ // if only one fnc export default 
+    return x+y
+}
+    function subtract (x ,y){
+    return x-y
+}
+export {add ,subtract}
+
+// export default
+//if only one fnc
